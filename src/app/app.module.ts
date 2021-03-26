6e6784f42f1d66f3354b1a1c876913bf4a6d8c49
Daniela Alonzo <monsterjunior@outlook.es>
2021-03-26 11:57:26 -0600

    Implementación de usuarios e inicialización de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HedearComponent } from './hedear/hedear.component';
+import { RegistroComponent } from './autenticacion/registro/registro.component';
+import { AutenticacionService } from './servicios/autenticacion.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  // { path: '**', component: InicioComponent },
+  { path: 'registro', component: RegistroComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HedearComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    // HttpModule
   ],
-  providers: [],
+  providers: [AutenticacionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/autenticacion/registro/registro.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  registroForm: FormGroup;
+  userdata: any;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registroForm = this.formBuilder.group({
+      'email': ['', [ Validators.required, Validators.email ]],
+      'password': ['', [
+        Validators.required,
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'), 
+        Validators.minLength(6)
+      ]]
+    })
+  }
+
+  onSubmit(){
+    this.userdata = this.saveUserdata();
+  }
+
+  saveUserdata(){
+    const saveUserdata = {
+      email: this.registroForm.get('email').value,
+      password: this.registroForm.get('password').value
+    }
+    return saveUserdata;
+  }
+
+}
